8d585419de6aaee127eaf6a798fa07bc6790f609
ayoroq <Ayoroq@gmail.com>
2024-03-26 13:40:47 -0400

    commiting changes

---
+++ AWS Three Tier Web Architecture using Boto3/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database.cluster-ctu46io8ibzj.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'dbpassword',
+    DB_DATABASE : 'webappdb'
+});
